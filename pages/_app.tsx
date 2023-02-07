5602994dee3a44aafceaf2bf83ae8c1e3f44f99c
moon <xmun74@naver.com>
2023-02-07 11:05:21 +0900

    chore: eslint rules, _app 정리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,15 @@
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
 import { RecoilRoot } from 'recoil'
-import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/GlobalStyle'
 import Layout from '../components/Layout'
-import { darkTheme, lightTheme } from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState('light')
-  const isDarkMode = theme === 'dark'
-
   return (
     <RecoilRoot>
-      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <GlobalStyle />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </RecoilRoot>
   )
 }
