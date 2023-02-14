2c9b8265108814aae381c6cdbb2e7e14c1a4b911
moon <xmun74@naver.com>
2023-02-14 13:05:15 +0900

    chore: react-markdown uninstall

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -1,4 +1,3 @@
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import styled from 'styled-components'
 
 const Content = styled.article`
@@ -47,23 +46,10 @@ const BodyH1 = styled.h1``
 const BodyH2 = styled.h2``
 const BodyImg = styled.img``
 
-const CustomComponents = {
-  h1({ ...props }) {
-    return <BodyH1>{props.children}</BodyH1>
-  },
-  h2({ ...props }) {
-    return <BodyH2>{props.children}</BodyH2>
-  },
-
-  img({ ...props }) {
-    return <BodyImg>{props.children}</BodyImg>
-  },
-}
-
 export default function PostBody({ content }: { content: string }) {
   return (
     <Content>
-      <ReactMarkdown components={CustomComponents}>{content}</ReactMarkdown>
+      <div>{content}</div>
     </Content>
   )
 }
